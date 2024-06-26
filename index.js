6957720a33ea412930178c2ef770c68abf144439
Siddhartha-Kolla <siddharthansg@gmail.com>
2024-06-26 22:05:42 +0200

    Added top button

--- index.js
+++ index.js
@@ -68,3 +68,24 @@ document.getElementById("question-form").addEventListener('submit', function(eve
 
 
 });
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
